b1885b2bdb01104918d50d63b2a9ac10e5273fec
srikanthms89 <srikanthms@tnsif.org>
2024-02-05 11:16:42 +0530

    First commit

---
+++ RevaEceBatch02/src/com/tnsif/daytweleve/MultiDimArrayDemo.java
@@ -0,0 +1,13 @@
+package com.tnsif.daytweleve;
+
+public class MultiDimArrayDemo {
+	public static void main(String[] args) {
+
+		int c[][] = { { 12, 45 }, { 10, 40 }, { 5, 7 } }; // new int[3][2];
+
+		System.out.println("No. of Rows in C array : " + c.length);
+
+		MLArray.printArray(c);
+	}
+
+}
